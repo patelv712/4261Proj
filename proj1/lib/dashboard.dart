ef9de835ed5835464ee182335535820ed63ea497
Varun Patel <varunpatel@Varuns-MacBook-Pro-2.local>
2024-02-13 20:35:12 -0500

    added nav bar

--- proj1/lib/dashboard.dart
+++ proj1/lib/dashboard.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0; // Current index for the bottom nav bar
+
+  static List<Widget> _widgetOptions = <Widget>[
+    Text('Home Screen', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Search', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Profile', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,7 +27,26 @@ class DashboardScreen extends StatelessWidget {
         title: Text('Dashboard'),
       ),
       body: Center(
-        child: Text('Welcome to the Dashboard!'),
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.deepPurple,
+        onTap: _onItemTapped,
       ),
     );
   }
